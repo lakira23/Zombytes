9441b5f9b8dd7145774f10721ef278516c6922bb
Lakira Weerasinghe <lakirar2020@gmail.com>
2025-11-10 20:58:22 +0000

    Introduced a enemy and its AI to follow the user, introduced health so that it decrease upon contact.

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import pygame
+import random
+import math
 from pygame.locals import *
 
 pygame.init()
 window = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Protoype 2")
+pygame.display.set_caption("Prototype 3")
 clock = pygame.time.Clock()
 
+enemy_x = random.randint(0, 750)
+enemy_y = random.randint(0, 750)
+enemy = pygame.Rect(enemy_x, enemy_y, 40, 40)
 player = pygame.Rect(375, 375, 50, 50)
 
+FPS = 60
 speed = 5
+enemy_speed = 2
 running = True
 
+player_health = 100
+
+minimum_distance = player.height
+
 while running:
-    clock.tick(60)
+    clock.tick(FPS)
     window.fill((30, 30, 30))
 
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
+    keys = pygame.key.get_pressed() #uses the keys to affect the user speed.
     if keys[K_w] or keys[K_UP]:
         player.y -= speed
     if keys[K_s] or keys[K_DOWN]:
@@ -31,8 +42,30 @@ while running:
 
     player.clamp_ip(window.get_rect())
 
-    pygame.draw.rect(window, (200, 100, 100), player)
-    pygame.display.update()
-    #prototype 2
+    # Vector movement with overlap prevention
+    dx = player.centerx - enemy.centerx
+    dy = player.centery - enemy.centery
+    distance = math.hypot(dx, dy)
 
-pygame.quit()
+    enemy_player_colision = pygame.Rect.colliderect(player, enemy)    
+
+    if player_health <= 0:
+        running = False
+    else:
+        if enemy_player_colision: player_health -= 1
+    
+    if not enemy_player_colision: 
+        dx, dy = dx / distance, dy / distance
+        enemy.x += dx * enemy_speed
+        enemy.y += dy * enemy_speed
+
+    print("health is ", player_health)
+
+    pygame.draw.rect(window, (200, 100, 100), player)
+    pygame.draw.rect(window, (255, 0, 0), enemy)
+    pygame.display.update()
+
+	#prototype 3
+     
+    
+pygame.quit() #quits the game
